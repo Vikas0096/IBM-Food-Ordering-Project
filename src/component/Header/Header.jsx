ab52dea0fc92d12a388ff61708f2127afa33dc66
Vikas Tiwari <vikastiwari0096@gmail.com>
2025-07-12 16:28:29 +0530

    change

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,10 +3,7 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-      <div className="header-contents">
-        <h2>"Satisfy Your Hunger, Elevate Your Mood."</h2>
-       
-      </div>
+    
     </div>
   )
 }
